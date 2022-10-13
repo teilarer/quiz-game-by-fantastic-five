78bfda9f1579aa801369e6a6022fb8b43386b5b4
pustot <maki.nwd@gmail.com>
2022-10-13 18:59:21 +0300

    добавлены линки для перехода на страницы

--- frontend/src/features/nav/Navigation.tsx
+++ frontend/src/features/nav/Navigation.tsx
@@ -1,4 +1,5 @@
 import { Tabs, Tab } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export default function Navigation ({user}) {
     return (
@@ -12,13 +13,13 @@ export default function Navigation ({user}) {
   >
      {!user ? (
         <>
-    <Tab value="Main Page" label="Main Page" />
-    <Tab value="Registration" label="Registration" />
-    <Tab value="Login" label="Login" />
+    <Tab value="Main Page" label="Main Page" href="/"/>
+    <Tab value="Registration" label="Registration" href="/registration"/>
+    <Tab value="Login" label="Login" href="/login"/>
     </>
     ) : (
         <>
-        <Tab value="Main Page" label="Main Page" />
+        <Tab value="Main Page" label="Main Page" href="/"/>
         <h3>
         Приветствую,
                 {user.name}
@@ -26,7 +27,7 @@ export default function Navigation ({user}) {
                 {' '}
                 {user.score}
         </h3>
-        <Tab value="Logout" label="Logout" />
+        <Tab value="Logout" label="Logout" href="/logout"/>
         </>
         )
     }
