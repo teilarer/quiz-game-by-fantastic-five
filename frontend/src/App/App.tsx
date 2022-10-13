7000ed8c87420d617331cc787eaa8325a172c95d
Max Popov <maxpopov19@icloud.com>
2022-10-13 16:08:17 +0300

    routes created

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import Main from '../features/main/Main';
+import Login from '../features/auth/Login';
+import Register from '../features/auth/Register';
+
+function App(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/features/main/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+function Main(): JSX.Element {
+  return (
+    <Outlet />
+  );
+}
+
+export default Main;
