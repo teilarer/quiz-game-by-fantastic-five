2b7450eac33e64c0431ac6a62bf4f87c04065dc2
pustot <maki.nwd@gmail.com>
2022-10-13 21:18:56 +0300

    Revert "Подклчен компонент в main"
    This reverts commit 980cf71a17951569ee411688eecbeb0dd36f30bd.
    
    M-U
    lu

--- frontend/src/features/main/Main.tsx
+++ frontend/src/features/main/Main.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navigation from '../nav/Navigation';
 
 function Main(): JSX.Element {
   return (
-    <>
-      <Navigation />
-      <Outlet />
-    </>
+    <Outlet />
   );
 }
 
--- frontend/src/features/nav/Navigation.tsx
+++ frontend/src/features/nav/Navigation.tsx
@@ -1,25 +1,25 @@
 import { Tabs, Tab } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-export default function Navigation({ user }) {
+export default function Navigation ({user}) {
     return (
 <nav>
   <Tabs
-    // value={value}
-    // onChange={handleChange}
+    value={value}
+    onChange={handleChange}
     textColor="secondary"
     indicatorColor="secondary"
     aria-label="secondary tabs example"
   >
      {!user ? (
         <>
-    <Tab value="Main Page" label="Main Page" href="/" />
-    <Tab value="Registration" label="Registration" href="/auth/register" />
-    <Tab value="Login" label="Login" href="auth/login" />
-        </>
+    <Tab value="Main Page" label="Main Page" href="/"/>
+    <Tab value="Registration" label="Registration" href="/registration"/>
+    <Tab value="Login" label="Login" href="/login"/>
+    </>
     ) : (
         <>
-        <Tab value="Main Page" label="Main Page" href="/" />
+        <Tab value="Main Page" label="Main Page" href="/"/>
         <h3>
         Приветствую,
                 {user.name}
@@ -27,10 +27,10 @@ export default function Navigation({ user }) {
                 {' '}
                 {user.score}
         </h3>
-        <Tab value="Logout" label="Logout" href="auth/logout" />
+        <Tab value="Logout" label="Logout" href="/logout"/>
         </>
-        )}
-  </Tabs>
-</nav>
-    );
-}
+        )
+    }
+    </Tabs>
+    </nav>
+    )}
