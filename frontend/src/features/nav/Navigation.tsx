6d84839d78d535f459e3bbaeb688dffafeabdc08
pustot <maki.nwd@gmail.com>
2022-10-14 11:22:06 +0300

    add css module

--- frontend/src/features/nav/Navigation.tsx
+++ frontend/src/features/nav/Navigation.tsx
@@ -1,39 +1,41 @@
 import { Tabs, Tab } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import style from './nav.module.css';
+import '@fontsource/roboto/100.css';
 
-export default function Navigation({ user }):JSX.Element {
+export default function Navigation({}): JSX.Element {
   const selector = useSelector((globalState: RootState) => globalState.auth);
-  console.log(selector, 'eto lejit selector');
-    return (
-<nav>
-  <Tabs
-    // value={value}
-    // onChange={handleChange}
-    textColor="secondary"
-    indicatorColor="secondary"
-    aria-label="secondary tabs example"
-  >
-     {!selector.user ? (
-        <>
-    <Tab value="Main Page" label="Main Page" href="/" />
-    <Tab value="Registration" label="Registration" href="/auth/register" />
-    <Tab value="Login" label="Login" href="/auth/login" />
-        </>
-    ) : (
-        <>
-        <Tab value="Main Page" label="Main Page" href="/" />
-        <h3>
-        Приветствую,
-                {selector?.user.name}
-                , ваш счёт
-                {' '}
-                {selector?.user.score}
-        </h3>
-        <Tab value="Logout" label="Logout" href="/logout" />
-        </>
+  return (
+    <div className={style.navbar}>
+      <Tabs
+        // value={value}
+        // onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+      >
+        {!selector.user ? (
+          <>
+            <Tab value="Main Page" label="Main Page" href="/" />
+            <Tab
+              value="Registration"
+              label="Registration"
+              href="/auth/register"
+            />
+            <Tab value="Login" label="Login" href="/auth/login" />
+          </>
+        ) : (
+          <>
+            <Tab value="Main Page" label="Main Page" href="/" />
+            <h5>
+              Приветствую,
+              {selector?.user.name}, ваш счёт {selector?.user.score}
+            </h5>
+            <Tab value="Logout" label="Logout" href="/logout" />
+          </>
         )}
-  </Tabs>
-</nav>
-    );
+      </Tabs>
+    </div>
+  );
 }
